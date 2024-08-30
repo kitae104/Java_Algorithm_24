7f4c717e4da061109c0b009f7056c45d0e486113
Kitae's Github <aqua0405@gmail.com>
2024-08-30 11:54:30 +0900

    240830-1154

--- src/ch01/FindMaximum.java
+++ src/ch01/FindMaximum.java
@@ -13,7 +13,7 @@ public class FindMaximum {
 
 	public static void main(String[] args) {
 		int[] intArray  =  {72, 60, 83, 47, 89, 95};
-		
+
 		int maximum = findMaximum(intArray);
 
 		System.out.println("최대값 = " + maximum);
@@ -29,9 +29,9 @@ public class FindMaximum {
 	 * @return
 	 */
 	private static int findMaximum(int[] arr) {
-		
+
 		int max = arr[0];		// 최대값을 첫 번째 요소로  정한다
-		
+
 		// 최대 숫자를 배열의 두 번째 요소부터 마지막 요소까지 한 번에 한 요소와 차례대로 비교한다.
      	// 만약 비교한 요소가 최대 숫자보다 크다면 최대 숫자를 그 요소로 변경한다.
      	for (int i = 1 ; i < arr.length ; i++)
