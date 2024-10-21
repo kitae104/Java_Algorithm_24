80bc8399fea8c61103edc25b5cb61eb285007b11
Kitae's Github <aqua0405@gmail.com>
2024-10-21 10:50:56 +0900

    Update AdjacencyMatrixMain.java

--- src/algorithm/graph/basic/matrix/AdjacencyMatrixMain.java
+++ src/algorithm/graph/basic/matrix/AdjacencyMatrixMain.java
@@ -5,20 +5,27 @@ import java.util.ArrayList;
 public class AdjacencyMatrixMain {
     public static void main(String[] args) {
         ArrayList<GraphNode> nodeList = new ArrayList<GraphNode>();
-        nodeList.add(new GraphNode("A", 0));
-        nodeList.add(new GraphNode("B", 1));
-        nodeList.add(new GraphNode("C", 2));
-        nodeList.add(new GraphNode("D", 3));
-        nodeList.add(new GraphNode("E", 4));
+        nodeList.add(new GraphNode("1", 0));
+        nodeList.add(new GraphNode("2", 1));
+        nodeList.add(new GraphNode("3", 2));
+        nodeList.add(new GraphNode("4", 3));
+        nodeList.add(new GraphNode("5", 4));
+        nodeList.add(new GraphNode("6", 4));
+        nodeList.add(new GraphNode("7", 4));
 
         Graph g = new Graph(nodeList);
 
-        g.addUndirectedEdge('A', 'B');
-        g.addUndirectedEdge('A', 'C');
-        g.addUndirectedEdge('A', 'D');
-        g.addUndirectedEdge('B', 'E');
-        g.addUndirectedEdge('C', 'D');
-        g.addUndirectedEdge('D', 'E');
+        g.addDirectedEdge('1', '2');
+        g.addDirectedEdge('1', '3');
+        g.addDirectedEdge('1', '7');
+        g.addDirectedEdge('2', '3');
+        g.addDirectedEdge('2', '4');
+        g.addDirectedEdge('3', '5');
+        g.addDirectedEdge('4', '3');
+        g.addDirectedEdge('5', '7');
+        g.addDirectedEdge('6', '5');
+        g.addDirectedEdge('7', '6');
+        g.addDirectedEdge('6', '4');
 
         System.out.println("==== 인접 행렬 ====");
         System.out.println(g);
