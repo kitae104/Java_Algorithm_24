22bdd139b25cc2514fd419368a1838ebaab99f65
Kitae's Github <aqua0405@gmail.com>
2024-10-21 22:47:09 +0900

    Update BinarySearchTree.java

--- src/algorithm/tree/bst/BinarySearchTree.java
+++ src/algorithm/tree/bst/BinarySearchTree.java
@@ -2,6 +2,10 @@ package algorithm.tree.bst;
 
 import lombok.Data;
 
+import javax.management.Query;
+import java.util.LinkedList;
+import java.util.Queue;
+
 @Data
 public class BinarySearchTree {
     private BinaryNode root;
@@ -72,9 +76,37 @@ public class BinarySearchTree {
      * @param node
      */
     public void levelOrder(BinaryNode node) {
-        if (node == null) {
-            return;
+        Queue<BinaryNode> queue = new LinkedList<>();   // 큐 생성
+        queue.add(root);
+        while(!queue.isEmpty()){
+            BinaryNode currentNode = queue.remove();
+            System.out.print(currentNode.getValue() + " ");
+            if(currentNode.getLeft() != null){
+                queue.add(currentNode.getLeft());   // 큐에 왼쪽 노드 추가
+            }
+            if(currentNode.getRight() != null){
+                queue.add(currentNode.getRight());  // 큐에 오른쪽 노드 추가
+            }
         }
+    }
 
+    /**
+     * 이진 탐색 트리에서 노드 검색
+     * @param node 현재 노드
+     * @param value 검색할 값
+     */
+    public BinaryNode search(BinaryNode node, int value) {
+        if(node == null) {  // 노드가 null이면 null 리턴
+            System.out.println("찾는 값 " + value + "가 트리에 없습니다.");
+            return null;
+        }
+        if(node.getValue() == value) {  // 노드의 값이 찾는 값과 같으면 노드 리턴
+            System.out.println("찾는 값 " + value + "이 트리에 있습니다.");
+            return node;
+        } else if(value < node.getValue()) { // 찾는 값이 현재 노드의 값보다 작으면 왼쪽 서브트리 탐색
+            return search(node.getLeft(), value);
+        } else {    // 찾는 값이 현재 노드의 값보다 크면 오른쪽 서브트리 탐색
+            return search(node.getRight(), value);
+        }
     }
 }
